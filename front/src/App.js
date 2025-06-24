233110bb3920bccd3f435031417fd14cab006611
seera <now-kirill@ya.ru>
2025-06-24 09:19:16 +0300

    Added uploader button and start auth page.

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import PdfUploaderComponent from "./component/pdfUploader";
+import AuthorizationComponent from "./component/authorizationComponent";
 
 function App() {
 
+    return (
+        <>
+            <Routes>
+                <Route path="/authorization" element={<AuthorizationComponent/>}/>
+                <Route path="/pdfUploader" element={<PdfUploaderComponent/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
---
+++ front/src/component/authorizationComponent/index.js
@@ -0,0 +1,11 @@
+const AuthorizationComponent = () => {
+
+    return (
+        <div>
+            {/* Здесь можно добавить разметку формы авторизации */}
+        </div>
+    )
+}
+
+
+export default AuthorizationComponent;
